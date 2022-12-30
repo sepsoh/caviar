0874a5ad0275e19bada52aa37fdb2d70c00f9ec8
Sepehr <sepehr7160@gmail.com>
2022-12-30 14:07:08 +0330

    create include file

---
+++ web/resources/views/include/searchbar.blade.php
@@ -0,0 +1,6 @@
+<div class="search-bar">
+    <form class="search-form d-flex align-items-center" method="POST" action="#">
+        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
+        <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+    </form>
+</div><!-- End Search Bar -->
---
+++ web/resources/views/include/sidebar.blade.php
@@ -0,0 +1,31 @@
+<ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/register">
+                <i class="bi bi-person"></i>
+                <span>Register</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/login">
+                <i class="bi bi-box-arrow-in-right"></i>
+                <span>Login</span>
+            </a>
+        </li><!-- End Login Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/contact">
+                <i class="bi bi-envelope"></i>
+                <span>Contact</span>
+            </a>
+        </li><!-- End Contact Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="/blog">
+                <i class="bi bi-file-earmark"></i>
+                <span>Blog</span>
+            </a>
+        </li><!-- End Blank Page Nav -->
+    </ul>
+
