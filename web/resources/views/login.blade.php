0d95358fab42a901f62b1bcde464d51c3324f96f
Sepehr <sepehr7160@gmail.com>
2022-12-30 14:08:38 +0330

    include logo

--- web/resources/views/login.blade.php
+++ web/resources/views/login.blade.php
@@ -8,8 +8,7 @@
 
                     <div class="d-flex justify-content-center py-4">
                         <a href="index.html" class="logo d-flex align-items-center w-auto">
-                            <img src="assets/img/logo.png" alt="">
-                            <span class="d-none d-lg-block">NiceAdmin</span>
+                            @include('include.logo')
                         </a>
                     </div><!-- End Logo -->
 
