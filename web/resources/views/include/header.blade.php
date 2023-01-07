bb9bd6cf29a996d55e9000ae3b0fa5dc223c796a
Sepehr <sepehr7160@gmail.com>
2023-01-07 19:21:27 +0330

    add login icon to header

--- web/resources/views/include/header.blade.php
+++ web/resources/views/include/header.blade.php
@@ -1,11 +1,13 @@
 <header id="header" class="header fixed-top d-flex align-items-center">
-
     <div class="d-flex align-items-center justify-content-between">
         <a href="/" class="logo d-flex align-items-center">
-            @yield('logo')
+            @include('include.logo')
         </a>
-        @yield('sidebar-buttons')
-    </div><!-- End Logo -->
-    @yield('searchbar')
+    </div>
+    <div class="d-flex align-items-center justify-content-between">
+        <a class="header-item d-flex align-items-center ri-account-circle-fill ri-2x" href="/login">
+        </a>
+    </div>
+    @include('include.searchbar')
 
-</header><!-- End Header -->
+</header>
