086f4f9ba4f78bf5016bc8cffa3bcda96881143b
Sepehr <sepehr7160@gmail.com>
2023-01-07 17:15:58 +0330

    initial commit

---
+++ web/resources/views/include/header.blade.php
@@ -0,0 +1,11 @@
+<header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+        <a href="/" class="logo d-flex align-items-center">
+            @yield('logo')
+        </a>
+        @yield('sidebar-buttons')
+    </div><!-- End Logo -->
+    @yield('searchbar')
+
+</header><!-- End Header -->
