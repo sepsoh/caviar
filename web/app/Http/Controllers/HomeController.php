2a2b66bd8685c3157a78c2b3d624d3c8339d1050
Sepehr <sepehr7160@gmail.com>
2023-01-09 02:05:44 +0330

    Add all returns to HomeContorller methods

--- web/app/Http/Controllers/HomeController.php
+++ web/app/Http/Controllers/HomeController.php
@@ -10,12 +10,12 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
-    public function root(): View
+    public function root(): View|Factory|Application
     {
         return view('home');
     }
 
-    public function search()
+    public function search() : View|Factory|Application
     {
         return view('home',['query'=>\request()->get('query')]);
     }
