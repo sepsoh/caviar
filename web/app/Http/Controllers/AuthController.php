84f7eb6adc4ebe41063904b9fca5dd3c15fa32ea
Sepehr <sepehr7160@gmail.com>
2023-02-15 10:57:57 +0330

    remove fake authentication

--- web/app/Http/Controllers/AuthController.php
+++ web/app/Http/Controllers/AuthController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\Authenticate;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use function Termwind\terminal;
 
 class AuthController extends Controller
@@ -20,25 +29,72 @@ class AuthController extends Controller
     }
     public function registerPost()
     {
-        if(true){
-            return view('login',['email'=> request()->get('email')]);
-        }else{
-            $registerErrors = [];
-            if(request()->get('password') != request()->get('confirm_password'))
-                $registerErrors[] = 'Password and confirm password does not match';
+
+
+         // this section must be edited
+
+        $registerErrors = [];
+        if(request()->input('password') != request()->input('confirm_password'))
+            $registerErrors[] = 'Password and confirm password does not match';
+
+        try {
+            $user = User::create([
+                'name' => request()->input('name') . ' ' . request()->input('lastname'),
+                'email' => request()->input('email'),
+                'password' => Hash::make(request()->input('password')),
+            ]);
+
+//            event(new Registered($user));
+//            Auth::login($user);
+        } catch (\Exception $e) {
+
+            $registerErrors[] = $e->getCode() == 23000 ? 'Email already exists' : 'Error while registering';
+        }
+
+
+        if(count($registerErrors) > 0){
 
             return view('register', [
                 'registerErrors'=> $registerErrors,
-            'registerData'=>[
-                'email'=>request()->input('email') ?? '',
-                'name'=>request()->input('name') ?? '',
-                'lastname'=>request()->input('lastname') ?? '',
-            ]]);
+                'registerData'=>[
+                    'email'=>request()->input('email') ?? '',
+                    'name'=>request()->input('name') ?? '',
+                    'lastname'=>request()->input('lastname') ?? '',
+                ]]);
+        }else{
+            return view('login',['email'=> request()->input('email')]);
         }
 
+
     }
-    public function loginPost()
+    public function loginPost(LoginRequest $request)
     {
-        return view('login');
+
+
+        $request->authenticate();
+
+        $request->session()->regenerate();
+        return redirect('/panel');
+        //return redirect()->intended(RouteServiceProvider::HOME);
+
+        /*if (Auth::attempt([
+            'email' => request()->input('email'),
+            'password' => Hash::make(request()->input('password'))
+        ])) {
+            dd('you are loged in');
+        }else{
+            dd('you are not loged in');
+        }*/
+
     }
+
+    public function logout(Request $request){
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
+
 }
